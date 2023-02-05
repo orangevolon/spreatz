1ad55b5759cdb2222764d610924df5dddfc66951
Amir Alami <amir.alami@shopify.com>
2023-02-05 10:57:07 +0100

    Open AI dep added.

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Config from 'react-native-config';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hi there</Text>
+      <Text>Here: {Config.OPEN_AI_API_KEY}</Text>
       <StatusBar style="auto" />
     </View>
   );
